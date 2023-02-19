2f2e5106435fe5982a3dede1473783d8bf32ba66
Mak-3 <75625675+Mak-3@users.noreply.github.com>
2023-02-19 20:49:34 +0530

    Add files via upload

---
+++ src/main/java/com/servlet/calculator_servlet.java
@@ -0,0 +1,37 @@
+package com.servlet;
+
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class calculator_servlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+
+        out.println("<html><head><title>Servlet CalculatorServlet</title></head><body>");               double n1 = Double.parseDouble(request.getParameter("txtN1"));
+
+
+        double n2 = Double.parseDouble(request.getParameter("txtN2"));
+        double result =0;
+        String opr=request.getParameter("operator");
+        if(opr.equals("+")){
+            result=n1+n2;
+        }
+        if(opr.equals("-")) {
+            result=n1-n2;
+        }
+        if(opr.equals("*")){
+            result=n1*n2;
+        }
+        if(opr.equals("/")) {
+            result=n1/n2;
+        }
+        out.println("<h1> Result = "+result);
+        out.println("</body></html>");
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Calculator App</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<h1>Calculator<h1>
+<form action="cal_servlet" method="get" class="form">
+Enter First Number
+<input type="text" name="txtN1" ><br> Enter Second Number
+<input type="text" name="txtN2" ><br><br>
+<input type="submit" name="operator" value="+">
+<input type="submit" name="operator" value="*">
+<input type="submit" name="operator" value="/">
+<input type="submit" name="operator" value="-">
+<br>
+<br> <input type="reset" value="clear">
+</form>
+</body>
+</html>
